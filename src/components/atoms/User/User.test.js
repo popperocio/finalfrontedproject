330563b70bcdb63a795250ce0a0d58bc60d890bb
rocio <rociocpoppe@gmail.com>
2024-04-08 12:27:10 -0300

    feat: add user icon component

---
+++ src/components/atoms/User/User.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { User } from './user';
+import '@testing-library/jest-dom'
+
+
+test('renders user icon', () => {
+  const { getByTestId } = render(<User/>);
+  const userIcon = getByTestId('user-icon');
+  expect(userIcon).toBeInTheDocument();
+  expect(userIcon).toHaveClass('MuiSvgIcon-root');
+});
---
+++ src/components/atoms/User/user.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+
+export const User = () => {
+  return (
+    <AccountCircleIcon fontSize="large" sx={{ color: "black "}} data-testid="user-icon"/>
+  )
+}
