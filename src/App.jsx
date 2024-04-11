00ad155f2f29d9da5576ec8d192d222b294933f7
rocio poppe <rocio.poppe@ioet.com>
2024-04-11 19:04:57 -0300

    feat: consume api to get hotels information

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import { Navbar } from './components/organisms/Navbar'
 import { Results } from './components/organisms/Results'
 import { SearchBar } from './components/organisms/SearchBar'
-import { SearchProvider } from './contexts/SearchContext'
+import { SearchProvider } from './contexts/SearchContext/SearchContext'
 
 function App() {
 
