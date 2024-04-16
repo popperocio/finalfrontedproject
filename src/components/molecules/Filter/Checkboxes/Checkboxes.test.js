37960c39ed1c9dacd1895c2c58b046a75059b475
rocio poppe <rocio.poppe@ioet.com>
2024-04-15 22:46:28 -0300

    fix import from Checkboxes test

--- src/components/molecules/Filter/Checkboxes/Checkboxes.test.js
+++ src/components/molecules/Filter/Checkboxes/Checkboxes.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom'
-import Checkboxes from './index';
+import { Checkboxes } from './index';
 import { SearchContext } from '../../../../contexts/SearchContext/SearchContext';
 
 describe('Checkboxes Component', () => {
