7a4d37ff4b4ea148db78b6f9050ec93570d792a1
rocio poppe <rocio.poppe@ioet.com>
2024-07-27 00:11:12 -0300

    add tests for calendar and search bar

--- src/components/atoms/Calendar/Calendar.test.js
+++ src/components/atoms/Calendar/Calendar.test.js
@@ -1,11 +1,47 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import { Calendar } from './index';
 import '@testing-library/jest-dom'
-
+import dayjs from 'dayjs';
 
 test('renders calendar', () => {
     const { getByPlaceholderText } = render(<Calendar/>);
     const inputField = getByPlaceholderText('MM/DD/YYYY');
     expect(inputField).toBeInTheDocument();
 });
+
+test('renders calendar and handles date change', () => {
+    const handleDateChange = jest.fn();
+    const { getByPlaceholderText } = render(<Calendar onDateChange={handleDateChange} />);
+    const inputField = getByPlaceholderText('MM/DD/YYYY');
+
+    fireEvent.change(inputField, { target: { value: '07/26/2024' } });
+
+
+    expect(handleDateChange).toHaveBeenCalled();
+  });
+
+test('renders calendar with minDate', () => {
+    const minDate = dayjs('2024-01-01'); 
+    const { getByPlaceholderText } = render(<Calendar minDate={minDate} />);
+    const inputField = getByPlaceholderText('MM/DD/YYYY');
+    
+    expect(inputField).toBeInTheDocument();
+  });
+
+  test('renders calendar without error styling when incompleteError is false', () => {
+    const { container } = render(
+      <Calendar 
+        label="Test Calendar"
+        selectedDate={dayjs()}
+        onDateChange={() => {}}
+        minDate={dayjs()}
+        incompleteError={false}
+      />
+    );
+  
+    const datePicker = container.querySelector('.MuiInputBase-root');
+
+    expect(datePicker).not.toHaveStyle('color: red'); 
+
+  });
--- src/components/organisms/SearchBar/Searchbar.test.js
+++ src/components/organisms/SearchBar/Searchbar.test.js
@@ -1,17 +1,43 @@
 import React from 'react';
-import { render, fireEvent, getByTestId} from '@testing-library/react';
+import { render, fireEvent, waitFor} from '@testing-library/react';
 import { SearchBar } from './index';
 import '@testing-library/jest-dom'
 import { SearchContext } from '../../../contexts/SearchContext/SearchContext';
+import userEvent from '@testing-library/user-event';
+import dayjs from 'dayjs';
 
-const mockSearchContextValues = {
-  searchData: {}, 
-  updateSearchData: jest.fn(),
-  searchedHotels: [],
-  setSearchPerformed: jest.fn()
-};
+let mockSearchContextValues;
 
+beforeEach(() => {
+  mockSearchContextValues = {
+    fromDate: '',
+    setFromDate: jest.fn(),
+    toDate: '',
+    setToDate: jest.fn(),
+    destination: '',
+    setDestination: jest.fn(),
+    travellers: '',
+    setTravellers: jest.fn(),
+    rooms: '',
+    setRooms: jest.fn(),
+    searchData: {
+      destination: '',
+      fromDate: '',
+      toDate: '',
+      travellers: 0,
+      rooms: 0,
+    },
+    updateSearchData: jest.fn((data) => {
+      mockSearchContextValues.searchData = {...data };
+    }),
+    setSearchPerformed: jest.fn(),
+    searchedHotels: [],
+    searchPerformed: false,
+    isBooking: false,
+  };
+});
 describe('SearchBar', () => {
+
   it('renders search bar', () => {
     const { getByTestId } = render(
       <SearchContext.Provider value={mockSearchContextValues}>
@@ -30,7 +56,7 @@ describe('SearchBar', () => {
     expect(searchButton).toBeInTheDocument();
   });
 
-  it('handleSearch updates correctly', () => {
+  it('handleSearch updates correctly', async() => {
     const { getByLabelText, getByText } = render(
       <SearchContext.Provider value={mockSearchContextValues}>
         <SearchBar />
@@ -42,13 +68,75 @@ describe('SearchBar', () => {
     const toDate = getByLabelText("To");
     const searchButton = getByText('Search');
 
-    fireEvent.change(destinationInput, { target: { value: 'New York' } });
-    fireEvent.change(fromDate, { target: { value: "Thu, 11 Apr 2025 03:00:00 GMT" } });
-    fireEvent.change(toDate, { target: { value: "Thu, 30 Apr 2025 03:00:00 GMT" } });
-    fireEvent.click(searchButton);
+    await userEvent.type(destinationInput, 'New York');
+    await userEvent.type(fromDate, '04/30/2025');
+    await userEvent.type(toDate, '05/01/2025');
+    await userEvent.click(searchButton);
 
     expect(destinationInput.value).toBe('New York');
     expect(mockSearchContextValues.setSearchPerformed).toHaveBeenCalledWith(true);
 
+    await waitFor(() => {
+      expect(mockSearchContextValues.updateSearchData).toHaveBeenCalledWith({
+        destination: destinationInput.value,
+        fromDate: "Wed, 30 Apr 2025 03:00:00 GMT",
+        toDate: "Thu, 01 May 2025 03:00:00 GMT",
+        travellers: 1,
+        rooms: 1
+      });
+    });
+
+   });
+  
+
+  it('handles date changes correctly', async() => {
+    const { getByLabelText, getByText } = render(
+      <SearchContext.Provider value={mockSearchContextValues}>
+        <SearchBar />
+      </SearchContext.Provider>
+    );
+    const destinationInput = getByLabelText('Destination');
+    const fromDateInput = getByLabelText('From');
+    const toDateInput = getByLabelText('To');
+    const searchButton = getByText('Search');
+    
+
+    await userEvent.type(destinationInput, 'New York');
+    await userEvent.type(fromDateInput, '04/30/2025');
+    await userEvent.type(toDateInput, '05/01/2025');
+    await userEvent.click(searchButton);
+
+    const originalFromDate = dayjs('04/30/2025', 'MM/DD/YYYY');
+    const originalToDate = dayjs('05/01/2025', 'MM/DD/YYYY');
+    const newFromDate = originalFromDate.add(1, 'day').format('MM/DD/YYYY');
+    const newToDate = originalToDate.add(2, 'day').format('MM/DD/YYYY');
+ 
+
+    await userEvent.type(fromDateInput, newFromDate);
+    await userEvent.type(toDateInput, newToDate);
+    await userEvent.click(searchButton);
+  
+    expect(mockSearchContextValues.updateSearchData).toHaveBeenCalled();
+    expect(mockSearchContextValues.searchData.fromDate).toBe("Thu, 01 May 2025 03:00:00 GMT");
+    expect(mockSearchContextValues.searchData.toDate).toBe("Sat, 03 May 2025 03:00:00 GMT");
+  });
+ it('sets searchPerformed to true when search is performed', async () => {
+    const { getByLabelText, getByText } = render(
+      <SearchContext.Provider value={mockSearchContextValues}>
+        <SearchBar />
+      </SearchContext.Provider>
+    );
+
+    const destinationInput = getByLabelText('Destination');
+    const fromDateInput = getByLabelText('From');
+    const toDateInput = getByLabelText('To');
+    const searchButton = getByText('Search');
+
+    await userEvent.type(destinationInput, 'New York');
+    await userEvent.type(fromDateInput, '04/30/2025');
+    await userEvent.type(toDateInput, '05/01/2025');
+    await userEvent.click(searchButton);
+
+    expect(mockSearchContextValues.setSearchPerformed).toHaveBeenCalledWith(true);
   });
 });
