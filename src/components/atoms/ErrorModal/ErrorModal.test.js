794d6a4bae387850cc4fed5d70b62aec96278480
rocio poppe <rocio.poppe@ioet.com>
2024-08-21 17:08:27 -0300

    add error modal for booking reservation

---
+++ src/components/atoms/ErrorModal/ErrorModal.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom'
+import ErrorModal from './index';
+
+describe('ErrorModal', () => {
+  test('renders when there is an error', () => {
+    const { getByText } = render(<ErrorModal open={true} onClose={() => {}} />);
+    const text = getByText(`We couldn't process the reservation. Please, try again`);
+    expect(text).toBeInTheDocument();
+  });
+
+  test('calls onClose when close button is clicked', () => {
+    const onCloseMock = jest.fn();
+    const { getByText } = render(<ErrorModal open={true} onClose={onCloseMock} />);
+    const closeButton = getByText('X');
+    fireEvent.click(closeButton);
+    expect(onCloseMock).toHaveBeenCalled();
+  });
+  
+  test('renders nothing when open is false', () => {
+    const { queryByText } = render(<ErrorModal open={false} onClose={() => {}} />);
+    const text = queryByText(`We couldn't process the reservation. Please, try again`);
+    expect(text).not.toBeInTheDocument();
+  });
+
+});
